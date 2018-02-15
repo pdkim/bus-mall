b0549f79934d14d15d321de7cb7b67e60d0a18fd
Phillip Kim <phildkim@gmail.com>
2018-02-15 09:37:39 -0800

    infinite loop.

--- app.js
+++ app.js
@@ -36,27 +36,35 @@ new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
 
 //images event
-var imgEl = document.getElementById('img1');
+var imgEl1 = document.getElementById('img1');
 var imgEl2 = document.getElementById('img2');
 var imgEl3 = document.getElementById('img3');
 
-imgEl.addEventListener('click', randomPic);
+imgEl1.addEventListener('click', randomPic);
 imgEl2.addEventListener('click', randomPic);
 imgEl3.addEventListener('click', randomPic);
+
 
 function randomPic() {
   var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
   var randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
   var randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
-  imgEl.src = Product.allProducts[randomIndex].filepath;
+  imgEl1.src = Product.allProducts[randomIndex].filepath;
   imgEl2.src = Product.allProducts[randomIndex2].filepath;
   imgEl3.src = Product.allProducts[randomIndex3].filepath;
+  var currentlyShowing = [imgEl1, imgEl2, imgEl3]
   //prevent repeating images within all 3 slots.
-  if(randomIndex === randomIndex2 || randomIndex === randomIndex3) {
-    randomPic();
+  while(currentlyShowing[0] !== -1) {
+    console.error('duplicate.  re-run');
+    currentlyShowing[0] = Product.allProducts[randomIndex].filepath;
   }
-  else if(randomIndex2 === randomIndex || randomIndex2 === randomIndex3) {
-    randomPic();
+  while(currentlyShowing[0] === currentlyShowing[1] || Product.justViewed.indexOf(currentlyShowing[1]) !== -1) {
+    console.error('duplicate. re-run');
+    currentlyShowing[1] = Product.allProducts[randomIndex2].filepath;
+  }
+  while(currentlyShowing[0] === currentlyShowing[2] || currentlyShowing[1] === currentlyShowing[2] || Product.justViewed.indexOf(currentlyShowing[2]) !== -1) {
+    console.error('duplicate.  re-run');
+    currentlyShowing[2] = Product.allProducts[randomIndex3].filepath;
   }
   //attempt to stop back to back images within same column
   
