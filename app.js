0e082bcb7c5624932ce6211ace9d699734929a08
Phillip Kim <phildkim@gmail.com>
2018-02-15 20:32:27 -0800

    I think the local storage is working, but the images are now epeating....

--- app.js
+++ app.js
@@ -150,10 +150,33 @@ function showTally() {
 Product.container.addEventListener('click', handleClick);
 
 //get storage data
-function getLocalStorage() {
-  var strProducts = localStorage.getItem('products');
-  var products = JSON.parse(strProducts);
-  
-}
+(function getLocalStorage() {
+  if(localStorage.products) {
+    var strProducts = localStorage.getItem('products');
+    var products = JSON.parse(strProducts);
+    for(var prod of products) {
+      var newProduct = new Product(prod.name, prod.path, prod.votes, prod.views);
+    }
+  }
+  else {
+    new Product('bag', 'img/bag.jpg');
+    new Product('banana', 'img/banana.jpg');
+    new Product('bathroom', 'img/bathroom.jpg');
+    new Product('boots', 'img/boots.jpg');
+    new Product('breakfast', 'img/breakfast.jpg');
+    new Product('bubblegum', 'img/bubblegum.jpg');
+    new Product('chair', 'img/chair.jpg');
+    new Product('cthulhu', 'img/cthulhu.jpg');
+    new Product('dog-duck', 'img/dog-duck.jpg');
+    new Product('dragon', 'img/dragon.jpg');
+    new Product('pen', 'img/pen.jpg');
+    new Product('pet-sweep', 'img/pet-sweep.jpg');
+    new Product('tauntaun', 'img/tauntaun.jpg');
+    new Product('unicorn', 'img/unicorn.jpg');
+    new Product('usb', 'img/usb.gif');
+    new Product('water-can', 'img/water-can.jpg');
+    new Product('wine-glass', 'img/wine-glass.jpg');
+  }
+})();
 
 displayPic();
